118be22a67983abdc067aabd26266be1901124c5
juanquinterodev <896@holbertonschool.com>
2019-06-20 20:55:46 +0000

    add 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+putchar(alpha);
+putchar('\n');
+return (0);
+}
