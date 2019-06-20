792953cc359face95aeabc9c07a19d7bdb6ab7e3
juanquinterodev <896@holbertonschool.com>
2019-06-20 21:40:22 +0000

    add all

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int dig;
+for (dig = 0; dig <= 9; dig++)
+{
+printf("%d", dig);
+}
+printf("\n");
+return (0);
+}
