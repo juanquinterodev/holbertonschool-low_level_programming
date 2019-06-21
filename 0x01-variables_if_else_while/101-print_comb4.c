f9119947dd97826c84552b089c5b70a20623d3a9
juanquinterodev <896@holbertonschool.com>
2019-06-20 23:14:34 -0500

    add 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+	for (num2 = 0; num2 < 10; num2++)
+	{
+	for (num3 = 0; num3 < 10; num3++)
+	{
+	if (num1 >= num2 || num1 >= num3 || num2 >= num3)
+	continue;
+	else
+	putchar ((num1 % 10) + '0');
+	putchar ((num2 % 10) + '0');
+	putchar ((num3 % 10) + '0');
+	if (num1 == 7 && num2 == 8 && num3 == 9)
+	{
+	}
+	else
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
