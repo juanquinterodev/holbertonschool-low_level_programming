0817f3ca572470cf23cea1c641554690733342c0
juanquinterodev <896@holbertonschool.com>
2019-06-20 22:43:00 -0500

    update 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-int num1, num2;
+	int number1, number2;
 
-for (num1 = 0; num1 < 10; num++)
-{
-for (num2 = 0; num2 < 10; num2++)
-{
-if (num1 >= num2)
-continue;
-else
-putchar ((num1 % 10) + '0');
-putchar ((num2 % 10) + '0');
-if (num1 == 8 && num2 == 9)
-{
+	for (number1 = 0; number1 < 10; number1++)
+	{
+	for (number2 = 0; number2 < 10; number2++)
+	{
+	if (number1 == number2 || number1 > number2)
+	continue;
+	else
+	putchar ((number1 % 10) + '0');
+	putchar ((number2 % 10) + '0');
+	if (number1 == 8 && number2 == 9)
+	{
+	}
+	else
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
-else
-{
-putchar (',');
-putchar (' ');
-}
-}
-}
-putchar('\n');
-return (0);
-
