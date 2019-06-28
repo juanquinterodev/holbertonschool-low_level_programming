72a19916299f1f6a229c646aa1b2e15cda4e9c3a
juanquinterodev <896@holbertonschool.com>
2019-06-28 04:14:57 +0000

    fix 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,27 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
+if (n > 1)
+{
+printf(" ");
+}
 if ((n % 3 == 0) && (n % 5 == 0))
 {
+printf("FizzBuzz");
+}
+else
 if ((n % 3) == 0)
+{
 printf("Fizz");
+}
 else
 if ((n % 5) == 0)
+{
 printf("Buzz");
 }
 else
-printf("FizzBuzz");
-}
 printf("%d", n);
+}
+putchar('\n');
 return (0);
 }
