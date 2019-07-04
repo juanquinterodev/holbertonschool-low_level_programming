8a928c0c967c6105afd10f967ba2bb5dea4ef013
juanquinterodev <896@holbertonschool.com>
2019-07-04 23:38:51 +0000

    add 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to s1 string
+ * @s2: pointer to s2 string
+ *
+ * Return: pointer to the string dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+int cs1, cs2;
+while (cs1 != '\0')
+cs1++;
+while (cs2 != '\0')
+cs2++;
+return (s1 - s2);
+}
