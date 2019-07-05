d2f2db27c23fd98670106a4211f79a1350dfd878
juanquinterodev <896@holbertonschool.com>
2019-07-05 03:27:06 +0000

    fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int cs1, cs2;
-while (cs1 != '\0')
-cs1++;
-while (cs2 != '\0')
-cs2++;
-return (s1 - s2);
+int cs1;
+
+for (cs1 = 0; s1[cs1] != '\0'; cs1++)
+{
+if (s1[cs1] == s2[cs1])
+;
+else
+return (s1[cs1] - s2[cs1]);
+}
+return (0);
 }
