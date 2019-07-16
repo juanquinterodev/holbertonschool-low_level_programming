ea012f2f1edc988a2722ab5e7614706b950f8364
juanquinterodev <896@holbertonschool.com>
2019-07-16 22:57:50 +0000

    add duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* *_strdup - duplicate a string
+* @str: parameter received
+* Return: Null otherwise the duplicated string
+*/
+char *_strdup(char *str)
+{
+char *c;
+unsigned int i = 0;
+unsigned int j;
+
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+{
+i++;
+}
+c = malloc(i * (sizeof(char)));
+
+if (c == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < i; j++)
+{
+c[j] = str[j];
+}
+return (c);
+}
