c0e1a130408c574821069343f2148a3513329552
juanquinterodev <896@holbertonschool.com>
2019-07-17 17:43:24 +0000

    fix duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* *_strdup - duplicate a string
+* @str: parameter received
+* Return: Null otherwise the duplicated string
+*/
+char *_strdup(char *str)
+{
+char *c;
+unsigned int i = 0;
+unsigned int j;
+
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+{
+i++;
+}
+c = malloc(i + 1* (sizeof(char)));
+
+if (c == NULL)
+{
+return (NULL);
+}
+for (j = 0; j <= i; j++)
+{
+c[j] = str[j];
+}
+c[j] = '\0';
+return (c);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+#endif
