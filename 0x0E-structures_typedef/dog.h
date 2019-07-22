d2ceaf8612cb85783d935d515798bcba1e9f8f6e
juanquinterodev <896@holbertonschool.com>
2019-07-22 17:08:45 +0000

    structures define

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+struct dog
+{
+
+/*
+ * struct dog - define struct
+ * @name: point to char
+ * @age: years float
+ * @owner: point to char
+ *
+ * Description: define struct
+ */
+
+char *name;
+float age;
+char *owner;
+}dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
