770e3b312557686a3ab2bd24e092cbf19d970f80
juanquinterodev <896@holbertonschool.com>
2019-07-22 17:40:27 +0000

    fix dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef _DOG_H_
+#define _DOG_H_
 struct dog
 {
 
@@ -9,13 +9,12 @@ struct dog
  * @age: years float
  * @owner: point to char
  *
- * Description: define struct
  */
 
 char *name;
 float age;
 char *owner;
-}dog;
+};
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
