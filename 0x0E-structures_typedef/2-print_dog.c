8a993dc04ef936e78f32211875e2c4be5b157dea
juanquinterodev <896@holbertonschool.com>
2019-07-22 20:51:30 +0000

    add space nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ if (d != NULL)
 {
 if (d->name == NULL)
 {
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 }
 else
 {
@@ -21,7 +21,7 @@ printf("Name: %s\n", d->name);
 }
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
-printf("Owner: (nil)");
+printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 }
