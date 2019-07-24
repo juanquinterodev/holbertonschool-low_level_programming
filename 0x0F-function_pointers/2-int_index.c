f0ec5a18b0c597072bdc50b7942e5fe17c93de16
juanquinterodev <896@holbertonschool.com>
2019-07-24 23:09:21 +0000

    add search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - match an integer
+ * @size: the size of array
+ * @array: array
+ * @cmp: function computer
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define _HOLBERTON_H_
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, unsigned int size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
