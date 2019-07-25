26f2580fcafe4a5bf0245b381b2c3e6233b5407d
juanquinterodev <896@holbertonschool.com>
2019-07-25 03:33:42 +0000

    fix 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Prints opcodes of the main function
+ * @argc: Amount arguments
+ * @argv: Value argument
+ *
+ * Return: Returns an int
+**/
 int main(int argc, char *argv[])
 {
 if (argc != 2)
-printf ("Error, \n", exit(1));
-if atoi (argv(1) < 0)
-printf ("Error, \n", exit (2));
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf ("Error\n");
+exit (2);
+}
 return (0);
 }
