a8cc414161a8701103113bbff4df03c2708f24f1
juanquinterodev <896@holbertonschool.com>
2019-08-07 03:15:30 +0000

    del head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* pop_listint - del head node
+*@head: first
+*@n: head nodes data
+*Return: The head nodes data n
+*/
+int pop_listint(listint_t **head)
+{
+int n;
+listint_t *tmp;
+
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+	n = tmp->n;
+	free (tmp);
+	*head = (*head)->next;
+	return (n);
+}
