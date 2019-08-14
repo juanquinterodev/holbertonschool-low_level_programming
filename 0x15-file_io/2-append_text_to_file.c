41a2e98a96a857e6c3575fb4c02cfb2f6dce124d
juanquinterodev <896@holbertonschool.com>
2019-08-14 04:25:07 +0000

    fix betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,12 +19,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		return (1);
-	
+
 	while (text_content[i] != '\0')
 		i++;
-	
+
 	check = write(fd, text_content, i);
-	
+
 	if (check == -1)
 		return (-1);
 
