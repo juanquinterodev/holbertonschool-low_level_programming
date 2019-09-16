cdb890127969af64d8f11147e5067f524a38f882
juanquinterodev <896@holbertonschool.com>
2019-09-15 23:05:23 -0500

    fix betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 /**
 * print_dlistint - prints all the elements of the list.
-*@h: the paremeter list.
-*Return: the number of nodes.
+* @h: the paremeter list.
+*
+* Return: the number of nodes.
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int sum = 0;
+int sum = 0;
 
-	if (h == NULL)
-	{
-		return (sum)
-	}
-	for (sum = 0; h != NULL; sum++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (sum);
+if (h == NULL)
+{
+return (sum)
+}
+for (sum = 0; h != NULL; sum++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (sum);
 }
