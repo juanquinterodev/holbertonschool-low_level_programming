ae63c1805114b62bd86fe62d4c7d36b853ba866d
juanquinterodev <896@holbertonschool.com>
2019-09-16 08:28:46 -0500

    fix del

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -39,7 +39,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 	del->prev->next = del->next;
-	if (del>next)
+	if (del->next)
 	{
 		del->next->prev = del->prev;
 	}
